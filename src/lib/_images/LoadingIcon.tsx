9b59d6a6c588051eb1bbfb907cb97f0ba934023d
Faruk Yerli <faruk.yerli@synchron.ua>
2021-10-10 13:47:22 +0300

    svg loading responsive problem fixed

--- src/lib/_images/LoadingIcon.tsx
+++ src/lib/_images/LoadingIcon.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import '../_styles/LoadingIcon.scss'
 
-export default (props: any) => <div id="circularG" {...props}>
-    <div id="circularG_1" className="circularG"></div>
-    <div id="circularG_2" className="circularG"></div>
-    <div id="circularG_3" className="circularG"></div>
-    <div id="circularG_4" className="circularG"></div>
-    <div id="circularG_5" className="circularG"></div>
-    <div id="circularG_6" className="circularG"></div>
-    <div id="circularG_7" className="circularG"></div>
-    <div id="circularG_8" className="circularG"></div>
-</div>
+export default (props: any) => (
+    <div {...props}>
+        <div id="circularG" >
+            <div id="circularG_1" className="circularG"></div>
+            <div id="circularG_2" className="circularG"></div>
+            <div id="circularG_3" className="circularG"></div>
+            <div id="circularG_4" className="circularG"></div>
+            <div id="circularG_5" className="circularG"></div>
+            <div id="circularG_6" className="circularG"></div>
+            <div id="circularG_7" className="circularG"></div>
+            <div id="circularG_8" className="circularG"></div>
+        </div>
+    </div>
+)
