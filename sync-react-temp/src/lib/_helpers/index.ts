c2e1b82941cad1ef5518ef91a03d98bb5c24611a
farukyerli <farukyerli@yahoo.com>
2021-10-14 17:04:19 +0300

    New Publish

--- sync-react-temp/src/lib/_helpers/index.ts
+++
@@ -1,4 +0,0 @@
-export const deepCopy = <T>(objectValue: T): T => {
-    // console.log('deepCopy objectValue', objectValue);
-    return objectValue ? JSON.parse(JSON.stringify(objectValue)) : objectValue;
-};
